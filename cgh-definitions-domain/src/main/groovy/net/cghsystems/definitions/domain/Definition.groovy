3ee3f64a7ba83f2f7aa159f6fb678ace4160f8ad
Christopher Hedley <chris@cghsystems.net>
2012-11-29 19:00:09 +0000

    Changed Definition.id from Long to String as mongo does not support Long ids, removed findByID and use generic findOne as provided by Spring-data, added test to prove that Mongo creates Ids

--- cgh-definitions-domain/src/main/groovy/net/cghsystems/definitions/domain/Definition.groovy
+++ cgh-definitions-domain/src/main/groovy/net/cghsystems/definitions/domain/Definition.groovy
@@ -6,7 +6,7 @@ import groovy.transform.TupleConstructor
 
 /**
  * The domain class that contain all of the properties required for a definition.
- * 
+ *
  * @author chris
  *
  */
@@ -18,6 +18,8 @@ class Definition {
     /** Empty implementation to enforce the null object pattern */
     static Definition EMPTY_DEFINITION = new Definition(name: "Empty", definition: "Empty", description: "Empty")
 
+    /** <b>NOTE</b> id must be first property for @TupleConstructor reasons */
     String id, name, definition, description
     Long definitionCategoryId
+
 }
